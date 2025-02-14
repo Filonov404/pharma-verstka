5c7376660dd6a5d00b3367ba45fb8b59df37c96f
aleksandrfilonov <fffilonov31@gmail.com>
2025-02-14 18:21:39 +0300

    Закончил вносить правки, 4 страницы готовы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+const burger = document.querySelector('.burger');
+const burgerSpan = document.querySelector('.burger span');
+const menu = document.querySelector('.header-menu');
+const menuItems = document.querySelectorAll('.header-menu > li');
+
+burger.addEventListener('click', () => {
+    document.body.classList.toggle("no-scroll");
+    menu.classList.toggle('open');
+    burgerSpan.classList.toggle('open-burger');
+});
+
+menuItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        if (window.innerWidth <= 1550) {
+            event.stopPropagation();
+            menuItems.forEach(i => {
+                if (i !== item) {
+                    i.classList.remove('open');
+                }
+            });
+            item.classList.toggle('open');
+        }
+    });
+});
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && !burger.contains(event.target)) {
+        menu.classList.remove('open');
+        menuItems.forEach(item => {
+            item.classList.remove('open');
+        });
+    }
+});
+
 $(".js-dropdown").each(function () {
     var $dropdown = $(this),
         $dropdownToggler = $dropdown.find(".js-dropdown-toggler"),
@@ -39,39 +72,13 @@ $(".js-lang").each(function () {
 $('.header-menu li:has(.dropdown-menu)').append($('<span class="dropdown-icon"><svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.99992 5.58586L12.2928 0.292969L13.707 1.70718L6.99992 8.41429L0.292818 1.70718L1.70703 0.292969L6.99992 5.58586Z" fill="#1A1A1E"/></svg></span>'));
 
 
-$(function () {
-    $('.has-dropdown a').click(function () {
-        $(this).next('.dropdown-menu').toggleClass('open');
-    });
-
-    // $(document).click(function (e) {
-    //     var target = e.target;
-    //     if (!$(target).is('.has-dropdown a') && !$(target).parents().is('.has-dropdown a')){ 
-    //         $('.dropdown-menu').fadeOut(); 
-    //     }
-    // });
-});
-
-$(".burger").click(function () {
-    $(".header-menu").toggleClass("open-menu");
-    $("body").toggleClass("no-scroll");
-    $(".burger span").toggleClass("open-burger");
-});
-$(".close-menu").click(function () {
-    $(".header-menu").removeClass("slide");
-});
-
 $("form").submit(function (event) {
     event.preventDefault();
-
-    //get the form data
     var formData = {
         name: $("input[name=name]").val(),
         email: $("input[name=email]").val(),
         tel: $("input[name=tel]").val()
     };
-
-    // process the form
     $.ajax({
         type: "POST",
         url: "//jsonplaceholder.typicode.com/posts",
@@ -81,16 +88,6 @@ $("form").submit(function (event) {
     }).done(function (data) {
         $('.form-inner').remove()
         $(".response")
-        // .empty()
-        // .append(JSON.stringify(data, null, 2));
         $('.response').append('<div class="sucscess"><div class="modal_title">Заявка принята!</div></div>');
-
     });
-
-
-    const items = $('.star-decor-el');
-    const half = Math.ceil(items.length / 2);
-    items.slice(half).hide();
-    console.log(items);
-
 });
